3880d3f7067a7e6cda3f50f5fcd0f99c7649a8f7
Nereus Chen <nereuschen0122@gmail.com>
2025-07-08 21:50:53 +0800

    Second Version

--- products.py
+++ products.py
@@ -6,3 +6,5 @@ while True:
 	price = input("請輸入商品價格:")
 	products.append([name, price]) # products[0][0] = products清單中第一個 index 中的第一個 index
 print(products)
+for p in products:
+	print(p[0], "的價格是", p[1])
