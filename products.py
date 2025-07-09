0e5c45f07f992e1b58ab28c096e273b6c228f5ea
Nereus Chen <nereuschen0122@gmail.com>
2025-07-09 13:08:34 +0800

    4th Version

--- products.py
+++ products.py
@@ -4,10 +4,15 @@ while True:
 	if name == 'q': # quit
 		break
 	price = input("請輸入商品價格:")
+	price = int(price) # int = interger 整數
 	products.append([name, price]) # products[0][0] = products清單中第一個 index 中的第一個 index
 print(products)
 for p in products:
 	print(p[0], "的價格是", p[1])
-with open('products.csv', 'w') as f:
+with open('products.txt', 'w', encoding = 'utf-8') as f: # 使用utf-8(最好用的，有支援全語言)來編碼
+	f.write("商品, 價格\n0") # 寫入和讀取時會出現編碼(encoding)問題
 	for p in products:
-		f.write(p[0] + ',' + p[1] + '\n')
+		f.write(str(p[0]) + ',' + str(p[1]) + '\n')
+# 但如果要用excel打開就會有錯誤，因為excel的編碼不是用utf-8，所以要去[ 資料 -> 取得外部資料 -> 從文字檔]
+# c記得delimiter(分隔號)要使用comma
+# add header 增加欄位名稱
